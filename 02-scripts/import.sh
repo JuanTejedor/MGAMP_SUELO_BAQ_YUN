7cddd6e26eb33cf4b2e5374f040543271ae2b6c1
JuanWSL <juan.a.t.s2001@hotmail.es>
2024-01-26 13:34:52 +0100

    nombre carpetas

--- 02-scripts/import.sh
+++ 02-scripts/import.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-
+mv 03-data/raw_data 03-data/00raw_data
 
 ## SUBMUESTREO ##
 # Submuestreamos los reads originales, tomando sólo el 10%. Antes creamos carpeta donde guardarlo. 
-mkdir 03-data/muxed_pe
+mkdir 03-data/01muxed_pe
 cp 03-data/raw_data/barcodes.fastq.gz 03-data/muxed_pe/
-seqtk sample -s100 03-data/raw_data/reverse.fastq.gz 0.1 > 03-data/muxed_pe/reverse_sub.fastq
-seqtk sample -s100 03-data/raw_data/forward.fastq.gz 0.1 > 03-data/muxed_pe/forward_sub.fastq
+seqtk sample -s100 03-data/00raw_data/reverse.fastq.gz 0.1 > 03-data/01muxed_pe/reverse_sub.fastq
+seqtk sample -s100 03-data/00raw_data/forward.fastq.gz 0.1 > 03-data/01muxed_pe/forward_sub.fastq
 
 ## IMPORTACIÓN ##
 # Para importar los datos debemos conocer la naturaleza de estos: sabemos que
