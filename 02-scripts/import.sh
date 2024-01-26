6642da6a4b2a90d319cf3c18a6635a54a7e28df2
JuanWSL <juan.a.t.s2001@hotmail.es>
2024-01-26 20:00:36 +0100

    IMPORTACIÓN SE Y PE (se modificó totalmente la forma de hacerlo)

--- 02-scripts/import.sh
+++ 02-scripts/import.sh
@@ -2,40 +2,30 @@
 
 ######## IMPORTACIÓN DE READS A QIIME ########
 
-
 ## REQUISITOS PREVIOS ##
 # Para que el siguiente script funcione correctamente es necesario tener creados
 # previamente todos los directorios del proyecto utilizando el script
 # workdir.sh. Tras ello, se deben importar los datos crudos en una carpeta
 # llamada "/raw_data" que se debe colocar dentro de la carpeta "/03-data".
+# Además, el formato de los FASTQ debe coincidir con el del protocolo EMP.
 
 ## PREPARACION DEL DIRECTORIO DE TRABAJO ##
-# El nombre de la carpeta con los datos crudos se cambia para facilitar el acceso y seguir un orden
-# cronologico.
+# El nombre de la carpeta con los datos crudos se cambia para facilitar el acceso.
 mv 03-data/raw_data 03-data/00raw_data
 # Movemos los metadatos a la carpeta de documentacion
 mv 03-data/00raw_data/sample-metadata.tsv 01-documentation
-
-
-## SUBMUESTREO ##
-# Submuestreamos los reads originales, tomando sólo el 10%. Antes creamos
-# carpeta donde guardarlos. 
-mkdir 03-data/01muxed_pe
-# Copiamos los barcodes a los datos submuestreados.
-cp 03-data/00raw_data/barcodes.fastq.gz 03-data/01muxed_pe/
-# Realizamos el submuestreo.
-seqtk sample -s100 03-data/00raw_data/reverse.fastq.gz 0.1 > 03-data/01muxed_pe/reverse.fastq
-seqtk sample -s100 03-data/00raw_data/forward.fastq.gz 0.1 > 03-data/01muxed_pe/forward.fastq
-# Comprimimos ambos archivos (para que tengan el formato adecuado para QIIME)
-gzip 03-data/01muxed_pe/reverse.fastq
-gzip 03-data/01muxed_pe/forward.fastq
-
+# Creamos una carpeta para PE y para SE, pues será necesaria para la función de importación de QIIME.
+mkdir 03-data/00raw_data/01_PE 03-data/00raw_data/02_SE
+mv 03-data/00raw_data/*fastq* 03-data/00raw_data/01_PE
+cp 03-data/00raw_data/01_PE/barcodes.fastq.gz 03-data/00raw_data/02_SE
+cp 03-data/00raw_data/01_PE/forward.fastq.gz 03-data/00raw_data/02_SE/sequences.fastq.gz
 
 ## EVALUACIÓN DE CALIDAD ## 
 # Ejecutamos FASTQC y guardamos los resultados en una carpeta.
-mkdir 04-results/01muxed_pe
-mkdir 04-results/01muxed_pe/fastqc
-fastqc 03-data/01muxed_pe/forward.fastq.gz 03-data/01muxed_pe/reverse.fastq.gz -o 04-results/01muxed_pe/fastqc/
+mkdir 04-results/01_PE 04-results/02_SE
+mkdir 04-results/01_PE/fastqc/ 04-results/02_SE/fastqc/
+fastqc 03-data/00raw_data/01_PE/forward.fastq.gz 03-data/00raw_data/01_PE/reverse.fastq.gz -o 04-results/01_PE/fastqc/
+fastqc 03-data/00raw_data/02_SE/sequences.fastq.gz -o 04-results/02_SE/fastqc/
 # Para forwards la calidad es muy buena (excepto en el último nucleotido),
 # mientras que para reverse es muy mala (excepto del 9 al 100). Además, hay
 # algunos forwards que en su extremo final tienen un poco de adaptador.
@@ -54,19 +44,18 @@ fastqc 03-data/01muxed_pe/forward.fastq.gz 03-data/01muxed_pe/reverse.fastq.gz -
 # usar el parámetro "EMPPairedEndSequences" y la siguiente función.
 qiime tools import \
   --type EMPPairedEndSequences \
-  --input-path 03-data/01muxed_pe \
-  --output-path 03-data/01muxed_pe/EMP_muxed_pe.qza
+  --input-path 03-data/00raw_data/01_PE \
+  --output-path 04-results/01_PE/import_EMP_muxed.qza
+# Imported 03-data/00raw_data/01_PE as EMPPairedEndDirFmt to 04-results/01_PE/import_EMP_muxed.qza
 
 # Además, como la calidad de los reverse reads era mala, vamos a importar
-# también los forward reads solo, mediante una importanción single end. Para
+# también los forward reads solo, mediante una importación single end. Para
 # ello, debemos crear otro directorio y hacer unos cuantos cambios.
-cp -r 03-data/01muxed_pe/ 03-data/02muxed_se/
-rm 03-data/02muxed_se/reverse.fastq.gz 03-data/02muxed_se/*qza
-mv 03-data/02muxed_se/forward.fastq.gz 03-data/02muxed_se/sequences.fastq.gz
 qiime tools import \
-  --type EMPSingleEndSequences \
-  --input-path 03-data/02muxed_se \
-  --output-path 03-data/02muxed_se/EMP_muxed_se.qza
+  --type EMPSingleEndSequences  \
+  --input-path 03-data/00raw_data/02_SE \
+  --output-path 04-results/02_SE/import_EMP_muxed.qza
+#Imported 03-data/00raw_data/02_SE as EMPSingleEndDirFmt to 04-results/02_SE/import_EMP_muxed.qza
 
 # Por tanto, tenemos 2 directorios, uno con la importación PE y otro con la
 # importación SE.
