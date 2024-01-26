b1eadc4044a0de4e4b7b428a5b29df2b55b6ef06
JuanWSL <juan.a.t.s2001@hotmail.es>
2024-01-26 20:06:22 +0100

    cambio en el nombre de scripts

--- 02-scripts/02demultiplex.sh
+++ 02-scripts/02demultiplex.sh
@@ -14,8 +14,16 @@ qiime demux emp-paired \
 --i-seqs 03-data/01muxed_pe/sequences_EMP_muxed_pe.qza \
 --m-barcodes-file 01-documentation/sample-metadata.tsv \
 --m-barcodes-column BarcodeSequence \
---o-per-sample-sequences 04-results/01muxed_pe/demuxed_pe/ \
---o-error-correction-details 04-results/01muxed_pe/demuxed_pe/errors/
+--p-rev-comp-mapping-barcodes \
+--o-per-sample-sequences 04-results/01muxed_pe/demuxed_pe/demux.qza \
+--o-error-correction-details 04-results/01muxed_pe/demuxed_pe/errors.qza \
+--verbose
 
 
-
+qiime demux emp-paired \
+--i-seqs 03-data/01muxed_pe/sequences_EMP_muxed_pe.qza \
+--m-barcodes-file 01-documentation/sample-metadata.tsv \
+--m-barcodes-column BarcodeSequence \
+--o-per-sample-sequences 04-results/01muxed_pe/demuxed_pe/demux.qza \
+--o-error-correction-details 04-results/01muxed_pe/demuxed_pe/errors.qza \
+--verbose
